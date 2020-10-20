cf92df8e36bec1ec68a7a258e1ac0005963f85d3
Ayaka Mikazuki <ayaka@mail.shn.hk>
2020-10-20 17:18:47 +0800

    Optimize

--- 201020课堂作业#检验整除#.cpp
+++ 201020课堂作业#检验整除#.cpp
@@ -1,13 +1,13 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
-int main(){
+int main() {
     int imp;
     cin >> imp;
-    cout << ((imp % 2 == 0) && (imp % 3 == 0)) << endl; // 均可整除
-    cout << ((imp % 2 == 0) || (imp % 3 == 0)) << endl; // 至少一个可整除
-    cout << ((imp % 2 == 0) != (imp % 3 == 0)); // 只有一个可整除
-    return 0;
+    cout << ((imp % 2 == 0) && (imp % 3 == 0)) << '\n';  // 均可整除
+    cout << ((imp % 2 == 0) || (imp % 3 == 0)) << '\n';  // 至少一个可整除
+    cout << ((imp % 2 == 0) != (imp % 3 == 0));          // 只有一个可整除
 }
 
-// 欢迎扫描repos里的二维码赞助我
+// 欢迎扫描 repo 里的二维码赞助我
